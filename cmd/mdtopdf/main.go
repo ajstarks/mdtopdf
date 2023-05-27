3faa38aa51865d1eb50c3fb04201d632dd60cda6
Anthony Starks <ajstarks@gmail.com>
2023-05-27 19:22:17 -0400

    update cmd/mdtopdf

--- cmd/mdtopdf/main.go
+++ cmd/mdtopdf/main.go
@@ -4,62 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 
 	"github.com/ajstarks/mdtopdf"
 )
 
-var input = flag.String("i", "", "Input text filename; default is os.Stdin")
-var output = flag.String("o", "", "Output PDF filename; requiRed")
-var help = flag.Bool("help", false, "Show usage message")
+var input = flag.String("i", "", "input markdown file (default is standard input")
+var output = flag.String("o", "", "output PDF file (required)")
+var tracefile = flag.String("t", "", "trace file")
+
+func die(err error) {
+	fmt.Fprintf(os.Stderr, "%v\n", err)
+	os.Exit(1)
+}
 
 func main() {
-
 	flag.Parse()
-
-	if *help {
-		usage("Help Message")
-	}
-
 	if *output == "" {
-		usage("Output PDF filename is required")
+		flag.PrintDefaults()
+		os.Exit(1)
 	}
-
 	// get text for PDF
 	var content []byte
 	var err error
 	if *input == "" {
 		content, err = ioutil.ReadAll(os.Stdin)
 		if err != nil {
-			log.Fatal(err)
+			die(err)
 		}
 	} else {
-		content, err = ioutil.ReadFile(*input)
+		content, err = os.ReadFile(*input)
 		if err != nil {
-			log.Fatal(err)
+			die(err)
 		}
 	}
-
-	pf := mdtopdf.NewPdfRenderer("", "", *output, "trace.log")
-	pf.Pdf.SetSubject("How to convert markdown to PDF", true)
-	pf.Pdf.SetTitle("Example PDF converted from Markdown", true)
-	pf.THeader = mdtopdf.Styler{Font: "Times", Style: "IUB", Size: 20, Spacing: 2,
-		TextColor: mdtopdf.Color{Red: 0, Green: 0, Blue: 0},
-		FillColor: mdtopdf.Color{Red: 179, Green: 179, Blue: 255}}
-	pf.TBody = mdtopdf.Styler{Font: "Arial", Style: "", Size: 12, Spacing: 2,
-		TextColor: mdtopdf.Color{Red: 0, Green: 0, Blue: 0},
-		FillColor: mdtopdf.Color{Red: 255, Green: 102, Blue: 129}}
-
+	pf := mdtopdf.NewPdfRenderer("", "", *output, *tracefile)
 	err = pf.Process(content)
 	if err != nil {
-		log.Fatalf("pdf.OutputFileAndClose() error:%v", err)
+		die(err)
 	}
-}
-
-func usage(msg string) {
-	fmt.Println(msg + "\n")
-	fmt.Print("Usage: convert [options]\n")
-	flag.PrintDefaults()
-	os.Exit(0)
 }
